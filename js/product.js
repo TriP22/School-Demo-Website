b5ac3e16b3425926f32eb34dc03e96a855d95224
TriP22 <60031832+TriP22@users.noreply.github.com>
2020-02-25 00:27:26 +0530

    Project initiated

---
+++ js/product.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav'
+
+                  
+    });
+  });
